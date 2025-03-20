c68f52ba34a90623fc9b2104477357f20e37bdec
Matt Haskell <matthew.haskell229@gmail.com>
2025-03-19 21:34:58 -0500

    init commit for webserver, with userService being main implementatin

---
+++ src/main/java/com/project/webserver/DataService.java
@@ -0,0 +1,13 @@
+package com.project.webserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class DataService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataService.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/webserver/RouteServiceTests.java
@@ -0,0 +1,13 @@
+package com.project.webserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteServiceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
