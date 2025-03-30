209fe600401f9d37bd7a2d7bacbf4cb88fcb3da5
aditilve <aditilve008@gmail.com>
2025-03-30 00:33:53 -0500

    Backup changes

---
+++ src/main/java/com/project/webserver/model/RouteRequest.java
@@ -0,0 +1,41 @@
+package com.project.webserver.model;
+
+public class RouteRequest {
+    private double originLatitude;
+    private double originLongitude;
+    private double destinationLatitude;
+    private double destinationLongitude;
+
+    // Getters and Setters
+    public double getOriginLatitude() {
+        return originLatitude;
+    }
+
+    public void setOriginLatitude(double originLatitude) {
+        this.originLatitude = originLatitude;
+    }
+
+    public double getOriginLongitude() {
+        return originLongitude;
+    }
+
+    public void setOriginLongitude(double originLongitude) {
+        this.originLongitude = originLongitude;
+    }
+
+    public double getDestinationLatitude() {
+        return destinationLatitude;
+    }
+
+    public void setDestinationLatitude(double destinationLatitude) {
+        this.destinationLatitude = destinationLatitude;
+    }
+
+    public double getDestinationLongitude() {
+        return destinationLongitude;
+    }
+
+    public void setDestinationLongitude(double destinationLongitude) {
+        this.destinationLongitude = destinationLongitude;
+    }
+}
